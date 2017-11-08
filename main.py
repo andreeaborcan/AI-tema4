ae3d3cc6529a9a36c5d72ac69977e94bd270d57c
Andreea Borcan <andreea_borcan96@yahoo.ro>
2017-11-08 22:00:45 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+from xml.etree import ElementTree as ET
+
+
 def pattern():
-  #aici scrii tu
-  return None
+	e = ET.parse('pattern.aiml').getroot()
+	i=0
+	L=[]
+	for pattern in e.findall('pattern'):
+		L.append(pattern.text)
+		i=i+1
+	return L
+	
+	return None
+
+print(pattern())
   
 
 # de aici incepe un fel de main
